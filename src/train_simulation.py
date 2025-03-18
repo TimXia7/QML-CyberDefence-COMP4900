b63a66b33bcad4cc99b9279b9435b1e2cf89d6d4
EvanMoore02 <eqmoore1@gmail.com>
2025-03-17 20:28:00 -0400

    Added new track, train2 uses QRL now, and can loop through multiple inputs for QLearning to see which one it learns best with

--- src/train_simulation.py
+++ src/train_simulation.py
@@ -25,7 +25,9 @@ class Node:
             neighbors.append(self.bypass)
         return neighbors
 
-class Track:
+
+
+class SimpleTrack:
     def __init__(self):
         self.nodes = {i: Node(i) for i in range(9)}
 
@@ -39,9 +41,54 @@ class Track:
         self.nodes[6].next = self.nodes[7]
         self.nodes[7].next = self.nodes[0]
 
-        # Initiate Bypass with optional bypass variable (node 8)
+        # Bypass
         self.nodes[2].bypass = self.nodes[8]
         self.nodes[8].next = self.nodes[6]
+class AdvancedTrack:
+    def __init__(self):
+        self.nodes = {i: Node(i) for i in range(20)}
+
+        # Core loop
+        self.nodes[0].next = self.nodes[1]
+        self.nodes[1].next = self.nodes[2]
+        self.nodes[2].next = self.nodes[3]
+        self.nodes[3].next = self.nodes[4]
+        self.nodes[4].next = self.nodes[5]
+        self.nodes[5].next = self.nodes[6]
+        self.nodes[6].next = self.nodes[7]
+        self.nodes[7].next = self.nodes[0]  # Complete loop
+
+        # First bypass (shortcut)
+        self.nodes[2].bypass = self.nodes[8]
+        self.nodes[8].next = self.nodes[6]  # Shortcut to node 6
+
+        # Second bypass (secondary loop)
+        self.nodes[4].bypass = self.nodes[9]
+        self.nodes[9].next = self.nodes[10]
+        self.nodes[10].next = self.nodes[11]
+        self.nodes[11].next = self.nodes[5]  # Rejoins main track at node 5
+
+        # Third bypass (branch off node 7)
+        self.nodes[7].bypass = self.nodes[12]
+        self.nodes[12].next = self.nodes[13]
+        self.nodes[13].next = self.nodes[14]
+        self.nodes[14].next = self.nodes[6]  # Rejoins main loop at node 6
+
+        # Dead-end path (branch off node 3)
+        self.nodes[3].bypass = self.nodes[15]
+        self.nodes[15].next = self.nodes[16]  # Dead end at node 16
+
+        # Fourth bypass (alternate short loop)
+        self.nodes[1].bypass = self.nodes[17]
+        self.nodes[17].next = self.nodes[18]
+        self.nodes[18].next = self.nodes[5]  # Shortcut to node 5
+
+        # Long path to test complexity
+        self.nodes[10].bypass = self.nodes[19]
+        self.nodes[19].next = self.nodes[2]  # Long path reconnecting to main loop
+
+
+
 
 
 # Train class for agents and adversaries 
@@ -82,18 +129,40 @@ def calculate_distance(train1, train2, track):
 # Start the movement of the trains until train1 gets back to node 0
 # IMPORTANT: parameter, take_bypass, determines if train1 takes the loop or not, 
 # function returns the distance between the 2 trains after the loop
-def simulate_train_loop(train1, train2, track, take_bypass):
+
+
+def simulate_train_loop_predictable(train1, train2, track, take_bypass):
     start_position = train1.current_node.position
+    
     while True:
-
-        # if (train1.current_node.position == 2 ):
-        #     if (take_bypass):
-        #         print("The agent decided to take the bypass!")
-        #     else:
-        #         print("The agent decided to take the loop!")
-
         train1.move(take_bypass)
-        train2.move(False)  
+        train2.move(False)
+
+        if train1.current_node.position == start_position:
+            break
+
+    return calculate_distance(train1, train2, track)
+
+
+def simulate_train_loop_random(train1, train2, track, take_bypass):
+    start_position = train1.current_node.position
+    
+    while True:
+        train1.move(take_bypass)
+        train2.move(random.choice([True, False]))
+
+        if train1.current_node.position == start_position:
+            break
+
+    return calculate_distance(train1, train2, track)
+
+
+def simulate_train_loop_qrl(train1, train2, track, take_bypass_agent, take_bypass_adversary):
+    start_position = train1.current_node.position
+    
+    while True:
+        train1.move(take_bypass_agent)
+        train2.move(take_bypass_adversary)
 
         if train1.current_node.position == start_position:
             break
